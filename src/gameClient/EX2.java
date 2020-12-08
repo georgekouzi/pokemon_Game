d5ccb9b9d9196beaef23ce77c503e755f1cca54d
George <57435555+georgekouzi@users.noreply.github.com>
2020-12-08 16:07:16 +0200

    creat EX2

---
+++ src/gameClient/EX2.java
@@ -0,0 +1,16 @@
+package gameClient;
+
+public class EX2 implements Runnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
